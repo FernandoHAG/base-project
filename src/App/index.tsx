ae633a0c550174ce5b4dde303555703118b2868e
Fernando <fehgomes@gmail.com>
2024-06-21 02:29:17 -0300

    update and add store

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { I18nextProvider } from "react-i18next";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import Router from "../Pages";
 import i18next from "../i18n/i18n";
-import { BrowserRouter } from "react-router-dom";
-import { I18nextProvider } from "react-i18next";
+import store from "../redux/store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <I18nextProvider i18n={i18next}>
-        <Router />
-      </I18nextProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <I18nextProvider i18n={i18next}>
+          <Router />
+        </I18nextProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
